e54ca10475dcc4d496ea19501d9045e3bbba8d01
ZebraAlgebra <r10221026@ntu.edu.tw>
2022-12-18 15:34:11 +0800

    a js file to record some site settings
    it is not that direct to have a cross-browser way to use custom json files, so instead we change to js.

---
+++ site_settings.js
@@ -0,0 +1,18 @@
+const video_link_data = {
+    "p1": {
+        "bsktbl(2)": "srzogkTBkTI",
+        "clssrm(1)": "GNNg1kC-g4w",
+        "clssrm(2)": "YCxa6ZKTYoE",
+        "office(2)": "7tpWiehnwWs",
+    },
+    "p2": {
+        "ptroom(1)": "fqhHVCDqkDc",
+        "ptroom(2)": "Av8Xw-SDP2M",
+    }, 
+    "p3": {
+        "bsktbl(0)": "zo3dsn_3Dxw",
+        "bsktbl(1)": "WTcIycKuTfQ",
+        "bsktbl(2)": "tWKppedOYZI",
+        "ptroom(2)": "Av8Xw-SDP2M",
+    },
+};
